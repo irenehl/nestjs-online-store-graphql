d7a18861a42613ea9c04c032f0aed7debbd9169f
irenehl <26irenelopez@gmail.com>
2023-06-27 08:58:53 -0600

    feat: remove unused controller

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
-import { CategoryController } from './category.controller';
 import { PrismaService } from '@config/prisma.service';
 import { CategoryResolver } from './category.resolver';
 
 @Module({
     providers: [CategoryService, PrismaService, CategoryResolver],
-    controllers: [CategoryController],
 })
 export class CategoryModule {}
