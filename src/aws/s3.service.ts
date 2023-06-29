71c789a4bc51991c42c088dd7c5446e0081492d5
irenehl <26irenelopez@gmail.com>
2023-06-29 00:52:43 -0600

    feat: add presigned url from upload image

--- src/aws/s3.service.ts
+++ src/aws/s3.service.ts
@@ -1,8 +1,5 @@
-import {
-    DeleteObjectCommand,
-    PutObjectCommand,
-    S3Client,
-} from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -13,51 +10,22 @@ export class S3Service {
         private configService: ConfigService
     ) {}
 
-    private buildFileName(originalName: string) {
-        return `${new Date().toISOString()}_${originalName}`;
+    private buildKeyFromFileName(fileName: string) {
+        return `${fileName}-${new Date().toISOString()}`;
     }
 
-    private buildObjectUrl(originalName: string) {
-        return `${this.configService.get<string>(
-            'AWS_BUCKET_HOST'
-        )}/${originalName}`;
-    }
-
-    async uploadFile(file: Express.Multer.File) {
-        const fileName = this.buildFileName(file.originalname);
-
+    async generatePresignedUrl(originalFileName: string) {
+        const Key = this.buildKeyFromFileName(originalFileName);
         const command = new PutObjectCommand({
             Bucket: this.configService.get<string>('AWS_BUCKET_NAME'),
-            Key: fileName,
-            Body: file.buffer,
-            Tagging: 'public=yes',
-            ContentType: file.mimetype,
+            Key,
         });
-
-        const result = await this.s3.send(command);
-        const url = this.buildObjectUrl(fileName);
 
         return {
-            result,
-            fileName,
-            url,
+            uploadUrl: await getSignedUrl(this.s3 as any, command as any, {
+                expiresIn: 3600,
+            }),
+            key: Key,
         };
-    }
-
-    async removeFile(fileName: string) {
-        const command = new DeleteObjectCommand({
-            Bucket: this.configService.get<string>('AWS_BUCKET_NAME'),
-            Key: fileName,
-        });
-
-        return this.s3.send(command);
-    }
-
-    async replaceFile(file: Express.Multer.File, fileName: string) {
-        const newFile = await this.uploadFile(file);
-
-        await this.removeFile(fileName);
-
-        return newFile;
     }
 }
--- src/mocks/s3.mock.ts
+++ src/mocks/s3.mock.ts
@@ -1,10 +1,11 @@
 import { S3Client } from '@aws-sdk/client-s3';
+import { S3Service } from '@aws/s3.service';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 export type Context = {
     s3: S3Client;
 };
 
-export type S3MockContext = DeepMockProxy<S3Client>;
+export type S3MockContext = DeepMockProxy<S3Service>;
 
 export const createS3Mock = () => mockDeep<S3Client>();
