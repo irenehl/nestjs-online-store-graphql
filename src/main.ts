c9ec506ed9a9f554cd6c1bb142ca2a9cb8a7fa5e
Daniela <26irenelopez@gmail.com>
2023-06-30 12:33:25 -0600

    fix: capitalized port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(process.env.port || 3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
